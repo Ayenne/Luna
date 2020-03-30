0038dd3ecc030877dd2810cc5b7552546b7f1476
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-03-30 15:16:57 +0200

    Remove unused serializer.

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -4,12 +4,6 @@ from rest_framework import serializers
 User = get_user_model()
 
 
-class FullUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
