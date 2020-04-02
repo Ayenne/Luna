ad01940edb4935a9649f2e05416ff7766497cde5
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-04-02 17:18:36 +0200

    Add simple restaurant page logic on frontend, add missing serializers to reviews and users on backend.

--- backend/apps/reviews/serializers/serializers_reviews.py
+++ backend/apps/reviews/serializers/serializers_reviews.py
@@ -3,10 +3,23 @@ from rest_framework.fields import SerializerMethodField
 
 from apps.reviews.models.models_reviews import Review
 from apps.users.serializers import UserSerializer
+from apps.restaurants.serializers import RestaurantSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     idUser = UserSerializer(read_only=True)
+    idRestaurant = RestaurantSerializer(read_only=True)
+    amount_of_likes = serializers.SerializerMethodField()
+    amount_of_comments = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_amount_of_likes(review):
+        return review.fk_Like_to_Review.all().count()
+
+    @staticmethod
+    def get_amount_of_comments(review):
+        return review.fk_Comment_to_Review.all().count()
+
 
     class Meta:
         model = Review
