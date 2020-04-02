67f1f36687f52af3e6f44f2e05e3007856751876
Team Keepers <team@keepizi.com>
2020-04-02 21:26:24 +0200

    Change URL for home

--- frontend/src/store/actions/home.js
+++ frontend/src/store/actions/home.js
@@ -1,6 +1,6 @@
 export const getBestRestaurants = () => async (dispatch) => {
-    const FAKEURL = "http://localhost:8000/backend/api/home/"
-    // const URL = "https://luna.propulsion-learn.ch/backend/api/home/"
+    // const FAKEURL = "http://localhost:8000/backend/api/home/"
+    const URL = "https://luna.propulsion-learn.ch/backend/api/home/"
 
     const apiBestRestaurants = await fetch(FAKEURL);
     const responseBestRestaurants = await apiBestRestaurants.json();
