cf00e28928730a712d636096d2e121c13cfea411
Team Keepers <team@keepizi.com>
2020-04-02 13:08:58 +0000

    Update frontend/src/components/App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
-import NavigationBar from './NavigationBar'
-import Footer from './Footer';
-import UserProfilePage from "./UserProfilePage";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import LoginForm from "./LoginForm";
 import Registration from "./RegistrationPage";
 import Restaurant from "./restaurant_page/Restaurant"
 import RegistrationCopy from "./RegistrationCopyPage";
-
+import UserProfileItem from "./UserProfile";
+import NavigationBar from './NavigationBar'
+import Footer from './Footer';
+import UserProfilePage from "./UserProfilePage";
 import Home from "./Home";
 
-import './App.css';
- import LoginForm from "./LoginForm";
- import Registration from "./RegistrationPage";
- import RegistrationCopy from "./RegistrationCopyPage";
- import UserProfileItem from "./UserProfile";
+
 
 class App extends Component {
     render() {
@@ -30,7 +27,7 @@ class App extends Component {
                             <Route exact={ true } path="/" component={Home} />
                             <Route exact={ true } path="/register" component={Registration} />
                             <Route exact={ true } path="/restaurant" component={Restaurant} />
-                            <Route exact={ true } path="/userprofile" component={UserProfilePage} />
+                            <Route exact={ true } path="/profile" component={UserProfilePage} />
                         </Switch>
                     <Footer/>
                 </BrowserRouter>
