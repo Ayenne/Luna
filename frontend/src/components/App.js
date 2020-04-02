cd2ff16948892bd38ae18cd5b3f47d3718c5fca8
Desislava Hvarchilkova <dhvarchilkova@gmail.com>
2020-04-02 14:40:38 +0200

    Userprofile and userProfileComments pages added

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,19 +2,21 @@
 import { NavLink } from "react-router-dom";
 import NavigationBar from './NavigationBar'
 import Footer from './Footer';
+import UserProfilePage from "./UserProfilePage";
 
 
 import './App.css';
  import LoginForm from "./LoginForm";
  import Registration from "./RegistrationPage";
  import RegistrationCopy from "./RegistrationCopyPage";
+ import UserProfileItem from "./UserProfile";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <NavigationBar />
-                <RegistrationCopy />
+                <UserProfilePage />
                 <Footer/>
 
             </div>
